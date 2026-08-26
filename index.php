bd0fb638b8520838967ec9c7072d5bd0ed56c3d9
silvio adamo <silvioadamo@icloud.com>
2026-08-26 18:33:33 +0200

    php_01_silvio_adamo

---
+++ index.php
@@ -0,0 +1,17 @@
+<!-- TRACCIA 1 -->
+<?php
+
+$integer = 5;
+$float = 4.5;
+$string = "stringa";
+$boolean = true;
+
+var_dump($integer);
+var_dump($float);
+var_dump($string);
+var_dump($boolean);
+
+const INTEGER = 5;
+const FLOAT = 4.5;
+const STRING = "stringa";
+const BOOLEAN = true;
